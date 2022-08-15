f10a4b0bccf517e2026177e3406b57760ed1b9c5
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-15 22:42:00 +0530

    added lineComparisonUC1 program

---
+++ LineComparisonUC1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComaprison_0pps
+{
+    internal class LineComparisonUC1
+    {
+        public void Distance()
+        {
+            int x1 = 1, y1 = 1, x2 = 3, y2 = 3;
+            double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            Console.WriteLine("Calculate the distance of give cartisian :" + distance);
+        }
+    }
+}
